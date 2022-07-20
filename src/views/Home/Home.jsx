75eb33ce0134769fc390087bc091037b262597b0
moraesvictor <moraesvictor@gmail.com>
2022-07-20 20:15:18 -0300

    feat: create card component

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,3 +1,15 @@
+import React from 'react';
+
+import { Card } from '../../components/Card';
+
 export const Home = () => {
-    return null;
-}
+  return (
+    <S.Wrapper>
+      <Card
+        title="Receitas"
+        srcImg="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+        description="Receitas de bolo, tortas, doces e muito mais!"
+      />
+    </S.Wrapper>
+  );
+};
