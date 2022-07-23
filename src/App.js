91272ba5b2bed9eaa3518b21d9715f46520af950
moraesvictor <moraesvictor@gmail.com>
2022-07-23 08:41:05 -0300

    fix: remove useless wrapper from App.js

--- src/App.js
+++ src/App.js
@@ -9,13 +9,6 @@ import { Home } from './views/Home';
 
 const LoginPage = styled(_LoginPage)``;
 
-const Wrapper = styled.div`
-  background-color: ${props => props.theme.colors.primary};
-  display: flex;
-  justify-content: center;
-  vertical-align: center;
-  height: 100vh;
-`;
 
 function App() {
   return (
@@ -23,10 +16,8 @@ function App() {
       <ThemeProvider theme={mainTheme}>
         <BrowserRouter>
           <Switch>
-            <Wrapper>
-              <Route exact path="/" component={Home} />
-            </Wrapper>
-            <Route exact path="/home" component={Home} />
+              <Route exact path="/" component={LoginPage} />
+              <Route exact path="/home" component={Home} />
           </Switch>
         </BrowserRouter>
       </ThemeProvider>
